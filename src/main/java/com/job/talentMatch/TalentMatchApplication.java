b862f8e52035517b6b000aa25051f4c078ada7b1
vaibhavimekhe <vaibhavimekhe@gmail.com>
2026-03-11 20:13:13 +0530

    first commit

---
+++ src/main/java/com/job/talentMatch/TalentMatchApplication.java
@@ -0,0 +1,13 @@
+package com.job.talentMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentMatchApplication.class, args);
+	}
+
+}
