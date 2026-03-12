cd912f3d35d71ca42737e6afe715cbb5ca65192b
vam-09 <vaibhavimekhe@gmail.com>
2026-03-12 22:00:37 +0530

    dependency commented out

--- src/main/java/com/job/talentMatch/TalentMatchApplication.java
+++ src/main/java/com/job/talentMatch/TalentMatchApplication.java
@@ -1,12 +1,15 @@
 package com.job.talentMatch;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class TalentMatchApplication {
 
 	public static void main(String[] args) {
+//		log.info("inside the function");
 		SpringApplication.run(TalentMatchApplication.class, args);
 	}
 
